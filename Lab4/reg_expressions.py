3a156bde1eaf64de41305e376af6a5eb297be913
janeta1 <janetagrigoras@gmail.com>
2025-03-28 22:01:58 +0200

    finished lab

---
+++ Lab4/reg_expressions.py
@@ -0,0 +1,32 @@
+from Lab4.lexer import *
+
+ex1 = 'u?N{2}(O|P){3}Q*R+'
+print("Example 1:")
+print(f"Regular expression: {ex1}")
+result = get_string(tokens(ex1))
+print(f"String generated: {result}")
+print("Steps: ")
+print_log_steps()
+
+print("\n                                     ...")
+
+ex2 = '(X|Y|Z){3}8+(9|0)'
+print("Example 2:")
+print(f"Regular expression: {ex2}")
+result = []
+for _ in range(5):
+    result.append(get_string(tokens(ex2)))
+print(f"Strings generated: {result}")
+
+print("\n                                     ...")
+
+ex3 = '(H|i)(J|K)L*N?'
+print("Example 3:")
+print(f"Regular expression: {ex3}")
+result = []
+for _ in range(5):
+    result.append(get_string(tokens(ex3)))
+print(f"Strings generated: {result}")
+
+
+
